323a20088bb463a9b2b6c16698c90a036bf95659
Nikonorov.M <maximnikonorov957@gmail.com>
2025-03-04 12:21:04 +0300

    К каждой функции были добавлены docstring

--- src/masks.py
+++ src/masks.py
@@ -1,4 +1,14 @@
 def get_mask_card_number(card_number):
+    """
+        Маскирует номер карты, оставляя видимыми первые шесть и последние четыре цифры.
+
+        Аргументы:
+        card_number (str или int): Номер карты, который необходимо замаскировать. Может содержать пробелы.
+
+        Возвращает:
+        str: Замаскированный номер карты в формате "XXXXXX  XXXX",
+            или пустую строку, если номер невалиден.
+        """
     card_number = str(card_number).replace(" ", "")
     if not card_number.isdigit() or len(card_number) < 10:
         return ""
@@ -10,6 +20,16 @@ def get_mask_card_number(card_number):
 
 
 def get_mask_account(account_number):
+    """
+      Маскирует номер счёта, оставляя видимыми только последние четыре цифры.
+
+      Аргументы:
+      account_number (str или int): Номер счёта, который необходимо замаскировать. Может содержать пробелы.
+
+      Возвращает:
+      str: Замаскированный номер счёта в формате "XXXX",
+          где X - последние четыре цифры, или пустую строку, если номер невалиден.
+      """
     account_number = str(account_number).replace(" ", "")
     if not account_number.isdigit() or len(account_number) < 4:
         return ""
